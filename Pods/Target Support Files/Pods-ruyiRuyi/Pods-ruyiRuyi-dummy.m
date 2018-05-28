29ab6387ed654804ace3d8fcff9256361b461c70
yaoyongmin <1020017499@qq.com>
2018-05-28 12:47:10 +0800

    Revert "首次提交"
    This reverts commit b5cec08a86f028471436b24ed6db4ed697958164.

--- Pods/Target Support Files/Pods-ruyiRuyi/Pods-ruyiRuyi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ruyiRuyi : NSObject
-@end
-@implementation PodsDummy_Pods_ruyiRuyi
-@end
--- Pods/Target Support Files/Pods-ruyiRuyiTests/Pods-ruyiRuyiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ruyiRuyiTests : NSObject
-@end
-@implementation PodsDummy_Pods_ruyiRuyiTests
-@end
